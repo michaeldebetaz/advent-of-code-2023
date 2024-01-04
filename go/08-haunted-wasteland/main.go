ab8c3dc8f478d8206dc3384e76ce091c613c1102
michaeldebetaz <netarion@outlook.com>
2024-01-04 21:06:27 +0100

    Finished exercice 08 Part 1

--- go/08-haunted-wasteland/main.go
+++ go/08-haunted-wasteland/main.go
@@ -13,6 +13,8 @@ type Node struct {
 	right string
 }
 
+type Nodes map[string]Node
+
 func main() {
 	filepath := os.Args[1]
 	file, err := os.Open(filepath)
@@ -29,24 +31,48 @@ func main() {
 	}
 
 	chunks := strings.Split(text, "\n\n")
-	navigation := chunks[0]
-	// network := chunks[1]
 
-	lines := strings.Split(navigation, "\n")
-	nodes := make(map[string]Node)
+	var navigation []rune
+	for _, r := range chunks[0] {
+		navigation = append(navigation, r)
+	}
+
+	lines := strings.Split(chunks[1], "\n")
+	nodes := make(Nodes)
 
 	for _, line := range lines {
 		parts := strings.Split(line, " = ")
-		parent := parts[0]
-		children := parts[1]
-		children = strings.ReplaceAll(children, "(", "")
-		children = strings.ReplaceAll(children, ")", "")
-		data := strings.Split(children, ", ")
-		left := data[0]
-		right := data[1]
-		nodes[parent] = Node{left, right}
+		if len(parts) == 2 {
+			parent := parts[0]
+			children := parts[1]
+			children = strings.ReplaceAll(children, "(", "")
+			children = strings.ReplaceAll(children, ")", "")
+			data := strings.Split(children, ", ")
+			left := data[0]
+			right := data[1]
+			nodes[parent] = Node{left, right}
+		}
 	}
 
 	fmt.Println(nodes)
 
+	steps := 0
+	key := "AAA"
+	for i := 0; key != "ZZZ"; i = (i + 1) % len(navigation) {
+		direction := navigation[i]
+		key = getNextKey(direction, nodes[key])
+		fmt.Printf("Direction: %v, Key: %v\n", string(direction), key)
+		steps++
+	}
+	fmt.Printf("Steps: %v\n", steps)
+}
+
+func getNextKey(r rune, node Node) string {
+	var key string
+	if r == 'L' {
+		key = node.left
+	} else {
+		key = node.right
+	}
+	return key
 }
